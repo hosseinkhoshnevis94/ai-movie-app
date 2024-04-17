3dc1b77169c366a50c69c0a998fb7f867d811618
Hossein <hossein.khoshnevis77@gmail.com>
2024-04-17 23:39:50 +0200

    fix build bug

--- src/store.js
+++ src/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {tmdbApi} from './assets/services/tmdb'
-import genreReducer from './assets/features/genreSlice'
-import categoryReducer from './assets/features/categorySlice'
-import searchReducer from './assets/features/searchSlice'
 import searchGenreCategorySliceReducer from './assets/features/searchGenreCategorySlice'
 import userReducer from './assets/features/auth'
 
